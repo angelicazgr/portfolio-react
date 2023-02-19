d2fa1ef95c03f832323477e049fdbf6a4064b733
Angelica <angelicazografou@gmail.com>
2023-02-19 19:19:42 +0200

    Add intro video

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 import NavBar from "../../components/NavBar/NavBar";
 
+import IntroVideo from "../../assets/intro.mp4";
+
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <div>Into video</div>
+      <div>
+        <video width="auto" height="auto" autoPlay loop muted>
+          <source src={IntroVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <div>
         <div>Project 1</div>
         <div>Project 2</div>
