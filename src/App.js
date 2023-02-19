8fee3d83daa1a9ee4d6948ab0df008956ad2144f
Angelica <angelicazografou@gmail.com>
2023-02-19 19:11:50 +0200

    Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import NavBar from "../../components/NavBar/NavBar";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <div>Into video</div>
+      <div>
+        <div>Project 1</div>
+        <div>Project 2</div>
+        <div>Project 3</div>
+        <div>Project 4</div>
+      </div>
+      <div>
+        <div>email</div>
+        <div>subject</div>
+        <div>message</div>
+        <div>send</div>
+      </div>
+      <div>
+        <div>footer</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
