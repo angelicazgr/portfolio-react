d1398b893ec2f3d603c08e40ad6549153e06336c
Angelica <angelicazografou@gmail.com>
2023-04-22 13:50:55 +0300

    Update navigation bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,10 +10,18 @@ const NavBar = () => {
     <div className="nav-bar-root">
       <img src={Logo} alt="Logo" className="logo" />
       <div className="nav-items">
-        <Link to="/">Home</Link>
-        <Link to="/photobook">Photobook</Link>
-        <div>Projects</div>
-        <div>Contact</div>
+        <Link to="/" className="nav-item">
+          Home
+        </Link>
+        <Link to="/projects" className="nav-item">
+          Projects
+        </Link>
+        <Link to="/contact" className="nav-item">
+          Contact
+        </Link>
+        <Link to="/about" className="nav-item">
+          About
+        </Link>
       </div>
     </div>
   );
