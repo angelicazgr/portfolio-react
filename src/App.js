47034fdcf118cd2a859c8dbe300acce5541a0515
Angelica <angelicazografou@gmail.com>
2023-04-23 16:31:43 +0300

    Add Capio page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Photobook from "./pages/Photobook/Photobook";
+import Capio from "./pages/Capio/Capio";
 import Quotes from "./pages/Quotes/Quotes";
 import Contact from "./pages/Contact/Contact";
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/photobook" element={<Photobook />} />
+        <Route path="/capio" element={<Capio />} />
         <Route path="/about" element={<About />} />
         <Route path="/quotes" element={<Quotes />} />
         <Route path="/contact" element={<Contact />} />
---
+++ src/pages/Capio/Capio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import NavBar from "../../components/NavBar/NavBar";
+import "./Capio.css";
+
+const Capio = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Capio;
