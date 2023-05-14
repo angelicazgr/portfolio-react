f1ee093391f07e77a91608d1d2ab062b2b6f99b2
Angelica <angelicazografou@gmail.com>
2023-05-14 20:16:47 +0300

    Add card projects

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,18 +18,35 @@ const Home = () => {
         <Video src={IntroVideo} />
       </div>
       <div className="home-project-container" id="projects">
+        <h2 className="home-project-header">Projects</h2>
         <div className="home-project-grid">
           <Link to="/photobook">
-            <div className="home-project-item">Photobook</div>
+            <div className="home-project-item">
+              <div className="home-project-item-title">Photobook</div>
+              <div className="home-project-item-name">529</div>
+              <div className="home-project-item-cta">click to view</div>
+            </div>
           </Link>
           <Link to="/capio">
-            <div className="home-project-item">Brand Indentity</div>
+            <div className="home-project-item">
+              <div className="home-project-item-title">Brand Indentity</div>
+              <div className="home-project-item-name">Capio</div>
+              <div className="home-project-item-cta">click to view</div>
+            </div>
           </Link>
           <Link to="/quotes">
-            <div className="home-project-item">Posters</div>
+            <div className="home-project-item">
+              <div className="home-project-item-title">Posters</div>
+              <div className="home-project-item-name">Quotes</div>
+              <div className="home-project-item-cta">click to view</div>
+            </div>
           </Link>
           <Link to="/motion">
-            <div className="home-project-item">Motion design</div>
+            <div className="home-project-item">
+              <div className="home-project-item-title">Motion design</div>
+              <div className="home-project-item-name">The 25th letter</div>
+              <div className="home-project-item-cta">click to view</div>
+            </div>
           </Link>
         </div>
       </div>
