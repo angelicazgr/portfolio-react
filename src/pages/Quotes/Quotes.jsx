14c6f88b9fe5812553ef2aea5e5f07fd36239a98
Angelica <angelicazografou@gmail.com>
2023-05-15 22:00:59 +0300

    Redesign quotes

--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import Zoom from "react-medium-image-zoom";
 
 import CarsonQuote from "../../assets/quotes/carson.jpg";
 import CocteauQuote from "../../assets/quotes/cocteau.jpg";
@@ -8,57 +7,68 @@ import RembrandtQuote from "../../assets/quotes/rembrandt.jpg";
 import WarholQuote from "../../assets/quotes/warhol.jpg";
 import NavBar from "../../components/NavBar/NavBar";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
+import "react-medium-image-zoom/dist/styles.css";
 import "./Quotes.css";
 
 const Quotes = () => {
   return (
     <div>
       <NavBar />
-      <div className="quotes-slider-container">
-        <Swiper
-          cssMode={true}
-          navigation={true}
-          pagination={true}
-          mousewheel={true}
-          keyboard={true}
-          centeredSlides={true}
-          loop={true}
-          zoom={true}
-          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-        >
-          <SwiperSlide>
-            <img
-              src={CarsonQuote}
-              alt="carson-quote"
-              className="quotes-slide-image"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src={CocteauQuote}
-              alt="cocteau-quote"
-              className="quotes-slide-image"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src={RembrandtQuote}
-              alt="rembrandt-quote"
-              className="quotes-slide-image"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src={WarholQuote}
-              alt="warhol-quote"
-              className="quotes-slide-image"
-            />
-          </SwiperSlide>
-        </Swiper>
+      <div className="quotes-container">
+        <div className="quotes-row">
+          <div className="quotes-card">
+            <div className="quotes-image-title">Carson</div>
+            <div className="quotes-image-description">
+              Inspired by the extraordinary style of David Carson
+            </div>
+            <Zoom>
+              <img
+                src={CarsonQuote}
+                alt="carson-quote"
+                className="quotes-image"
+              />
+            </Zoom>
+          </div>
+          <div className="quotes-card">
+            <div className="quotes-image-title">Cocteau</div>
+            <div className="quotes-image-description">
+              A different way to portray reflection
+            </div>
+            <Zoom>
+              <img
+                src={CocteauQuote}
+                alt="cocteau-quote"
+                className="quotes-image"
+              />
+            </Zoom>
+          </div>
+        </div>
+        <div className="quotes-row">
+          <div className="quotes-card">
+            <div className="quotes-image-title">Sagmeister</div>
+            <div className="quotes-image-description">
+              Combining sketching with graphic design
+            </div>
+            <Zoom>
+              <img
+                src={RembrandtQuote}
+                alt="rembrandt-quote"
+                className="quotes-image"
+              />
+            </Zoom>
+          </div>
+          <div className="quotes-card">
+            <div className="quotes-image-title">Warhol</div>
+            <div className="quotes-image-description">Warhol and the media</div>
+            <Zoom>
+              <img
+                src={WarholQuote}
+                alt="warhol-quote"
+                className="quotes-image"
+              />
+            </Zoom>
+          </div>
+        </div>
       </div>
     </div>
   );
