b41df5c715ceab5cd5eea90501f4cc51f39f609c
Angelica <angelicazografou@gmail.com>
2023-06-02 10:29:44 +0300

    add new font and change project styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,30 +22,32 @@ const Home = () => {
         <div className="home-project-grid">
           <Link to="/photobook">
             <div className="home-project-item">
-              <div className="home-project-item-title">Photobook</div>
-              <div className="home-project-item-name">529</div>
-              <div className="home-project-item-cta">click to view</div>
+              <div className="home-project-item-title">529</div>
+              {/*} <div className="home-project-item-name"></div>*/}
+              <div className="home-project-item-cta">Photobook</div>
             </div>
           </Link>
           <Link to="/capio">
             <div className="home-project-item">
-              <div className="home-project-item-title">Brand Indentity</div>
-              <div className="home-project-item-name">Capio</div>
-              <div className="home-project-item-cta">click to view</div>
+              <div className="home-project-item-title">Capio</div>
+              {/*<div className="home-project-item-name"></div>*/}
+              <div className="home-project-item-cta">Brand Indentity</div>
             </div>
           </Link>
           <Link to="/quotes">
             <div className="home-project-item">
-              <div className="home-project-item-title">Posters</div>
-              <div className="home-project-item-name">Quotes</div>
-              <div className="home-project-item-cta">click to view</div>
+              <div className="home-project-item-title">Quotes</div>
+              {/* <div className="home-project-item-name">Posters</div> */}
+              <div className="home-project-item-cta">Posters</div>
             </div>
           </Link>
           <Link to="/motion">
             <div className="home-project-item">
-              <div className="home-project-item-title">Motion design</div>
-              <div className="home-project-item-name">The 25th letter</div>
-              <div className="home-project-item-cta">click to view</div>
+              <div className="home-project-item-title">The 25th letter</div>
+              {/*<div className="home-project-item-name"></div>*/}
+              <div className="home-project-item-cta">
+                Expiramental typography
+              </div>
             </div>
           </Link>
         </div>
