5d13c2cadd3cc252327fbf542f51b5582b05807f
Angelica <angelicazografou@gmail.com>
2023-06-02 13:31:29 +0300

    change about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,6 +7,15 @@ import illustrator from "../../assets/adobe icons/Illustrator.svg";
 import inDesign from "../../assets/adobe icons/InDesign.svg";
 import afterEffects from "../../assets/adobe icons/AfterEffects.svg";
 import figma from "../../assets/adobe icons/Figma.svg";
+import htmlIcon from "../../assets/tech-icons/html.svg";
+import cssIcon from "../../assets/tech-icons/css.png";
+import jsIcon from "../../assets/tech-icons/js.png";
+import reactIcon from "../../assets/tech-icons/react.png";
+import gitIcon from "../../assets/tech-icons/git.png";
+import greece from "../../assets/flags/gr.svg";
+import turkiye from "../../assets/flags/tr.svg";
+import greatBrit from "../../assets/flags/gb.svg";
+import timeline from "../../assets/timeline.png";
 import "./About.css";
 
 const About = () => {
@@ -42,41 +51,57 @@ const About = () => {
           <p>angelicazografou@gmail.com</p>
           <p>Thessaloniki,Greece</p>
         </div>
-        <div>
-          <p className="profile-accent">Education</p>
-          <p>2021-2024, Graphic design, AKTO college, Greece</p>
-          <p>2011-2013, Customs brokerage, Ministry of Economics, Greece</p>
-          <p>2006-2011, Software engineering, Tei Serron, Greece</p>
-        </div>
 
         <div>
           <p className="profile-accent">Techincal Skills</p>
-          <p>HTML, CSS, Javascript, React, Git, GitHub</p>
-          <img src={photoshop} alt="photoshop icon" className="adobe-icon" />
+          <img src={htmlIcon} alt="html icon" className="skills-icon" />
+          <img src={cssIcon} alt="css icon" className="skills-icon" />
+          <img src={jsIcon} alt="js icon" className="skills-icon" />
+          <img src={reactIcon} alt="react icon" className="skills-icon" />
+          <img src={gitIcon} alt="git icon" className="skills-icon" /> <br></br>
+          <img src={photoshop} alt="photoshop icon" className="skills-icon" />
           <img
             src={illustrator}
             alt="illustrator icon"
-            className="adobe-icon"
+            className="skills-icon"
           />
-          <img src={inDesign} alt="inDesign icon" className="adobe-icon" />
+          <img src={inDesign} alt="inDesign icon" className="skills-icon" />
           <img
             src={afterEffects}
             alt="after Effects icon"
-            className="adobe-icon"
+            className="skills-icon"
           />
-          <img src={figma} alt="figma icon" className="adobe-icon" />
+          <img src={figma} alt="figma icon" className="skills-icon" />
         </div>
         <div>
           <p className="profile-accent">Languages</p>
-          <p>Greek &#9866; Native </p>
-          <p>Turkish &#9866; Proficient </p>
-          <p>English &#9866; Intermediate </p>
+          <p>
+            <img src={greece} alt="Greek flag" className="flag-icon" />
+            Greek &#9866; Native{" "}
+          </p>
+          <p>
+            <img src={turkiye} alt="Turkiye flag" className="flag-icon" />
+            Turkish &#9866; Proficient{" "}
+          </p>
+          <p>
+            <img src={greatBrit} alt="Greek flag" className="flag-icon" />
+            English &#9866; Intermediate
+          </p>
         </div>
         <div>
           <p className="profile-accent">Soft Skills</p>
           <p>Problem-solving, Time management, Critical thinking,</p>
           <p>Decision-making, Organizational, Adaptability, Creativity,</p>
           Resourcefulness
+        </div>
+        <div>
+          <p className="profile-accent">Education</p>
+          <img src={timeline} alt="education timeline" className="education" />
+          {/*<p>
+            2021-2024, Graphic design, <br></br>AKTO college, Greece
+          </p>
+          <p>2011-2013, Customs brokerage, Ministry of Economics, Greece</p>
+  <p>2006-2011, Software engineering, Tei Serron, Greece</p>*/}
         </div>
         <div>
           <p className="profile-accent">Work experience</p>
